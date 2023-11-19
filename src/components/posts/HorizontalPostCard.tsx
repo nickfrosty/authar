9c92d9ea04c32e989077270b06dca8694404826e
nickfrosty <nfrostbutter@gmail.com>
2023-11-19 11:37:09 -0500

    feat: added horizontal post card

---
+++ src/components/posts/HorizontalPostCard.tsx
@@ -0,0 +1,65 @@
+import { memo } from "react";
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+import { FormattedDateAgo } from "@/components/core/FormattedDateAgo";
+import styles from "@/styles/posts/HorizontalPostCard.module.css";
+// import { Clock } from "react-feather";
+
+type HorizontalPostCardProps = {
+  title: string;
+  href: string;
+  date?: string;
+  description?: string;
+  imageSrc?: StaticImageData;
+  imageAlt?: string;
+};
+
+export const HorizontalPostCard = memo(
+  ({
+    title,
+    href,
+    description,
+    date,
+    imageSrc,
+    imageAlt,
+  }: HorizontalPostCardProps) => {
+    return (
+      <div className={styles.card}>
+        {!!imageSrc && (
+          <Link href={href} className={styles.imageLeft}>
+            <Image
+              src={imageSrc}
+              fill
+              alt={imageAlt ?? title}
+              title={imageAlt ?? title}
+              // priority={true}
+            />
+          </Link>
+        )}
+
+        <div className={styles.details}>
+          <h4 className={styles.title}>
+            <Link href={href}>{title}</Link>
+          </h4>
+
+          <section className={styles.meta}>
+            {/* {!!duration && (
+              <p className={styles.minor}>
+                <Clock strokeWidth={1.1} />
+                {duration}
+              </p>
+            )} */}
+
+            {!!date && (
+              <FormattedDateAgo date={date} className={styles.minor} />
+            )}
+          </section>
+
+          {!!description && (
+            <p className={`${styles.description} fade-bottom`}>{description}</p>
+          )}
+        </div>
+      </div>
+    );
+  },
+);
