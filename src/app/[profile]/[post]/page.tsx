30415704bd0e5c12a467abdc368f4f84f8a4b7fc
nickfrosty <nfrostbutter@gmail.com>
2023-11-18 22:41:13 -0500

    refactor: consume static post data

--- src/app/[profile]/[post]/page.tsx
+++ src/app/[profile]/[post]/page.tsx
@@ -2,7 +2,7 @@ import { Metadata, ResolvingMetadata } from "next";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-import { STATIC_USER } from "@/data";
+import { STATIC_POST } from "@/data";
 
 // import { serialize } from "next-mdx-remote/serialize";
 // import MarkdownFormatter from "@/components/MarkdownFormatter";
@@ -22,7 +22,7 @@ export async function generateMetadata(
 ): Promise<Metadata> {
   // locate the single record
   // locate the current post being requested
-  const post: any = null;
+  const post = STATIC_POST;
 
   // do nothing if the post was not found
   if (!post) return {};
@@ -30,17 +30,17 @@ export async function generateMetadata(
   // get the parent's images and add the post specific ones to be the first
   const openGraphImages = (await parent).openGraph?.images || [];
   if (post.image) {
-    openGraphImages.unshift(`/${"#todo"}/opengraph-image`);
+    openGraphImages.unshift(`${post.href}/opengraph-image`);
   }
 
   return {
     alternates: {
       canonical: post.href,
     },
-    title: `title`,
+    title: `${post.title} by ${post.author.name}`,
     description: post.description,
     openGraph: {
-      title: `title`,
+      title: `${post.title} by ${post.author.name}`,
       description: post.description,
       // note: `images` will be auto populated by the `opengraph-image` generator
       images: openGraphImages,
@@ -70,22 +70,22 @@ export default async function Page({ params }: PageProps) {
   return (
     <main className="page-container max-w-3xl !space-y-4 md:!space-y-6">
       <h1 className="font-bold text-3xl md:text-4xl max-w-5xl">
-        <Link href={`#`} className="">
-          Post title
+        <Link href={STATIC_POST.href} className="">
+          {STATIC_POST.title}
         </Link>
       </h1>
 
       <section className="flex items-center justify-between gap-4">
         <section className="flex items-center gap-2 md:gap-4">
           <Link
-            href={`/${STATIC_USER.username}`}
+            href={`/${STATIC_POST.author.username}`}
             className="block rounded-full overflow-hidden w-12 h-12 bg-slate-300"
           >
             <Image
               width={64}
               height={64}
-              src={STATIC_USER.image}
-              alt={STATIC_USER.name}
+              src={STATIC_POST.author.image}
+              alt={STATIC_POST.author.name}
               className="object-cover object-center rounded-full overflow-hidden w-12 h-12"
               priority={true}
             />
@@ -93,20 +93,20 @@ export default async function Page({ params }: PageProps) {
 
           <div className="space-y-0">
             <Link
-              href={`/${STATIC_USER.username}`}
+              href={`/${STATIC_POST.author.username}`}
               className="hover:underline md:text-lg font-medium"
             >
-              {STATIC_USER.name}
+              {STATIC_POST.author.name}
             </Link>
 
-            <FormattedDateAgo date={new Date().toDateString()} />
+            <FormattedDateAgo date={STATIC_POST.date} />
           </div>
 
           <button className="btn mx-8 border-gray-400">Following</button>
         </section>
 
         <section className="flex items-center gap-3">
-          <SocialShareButtons href={`#todo`} />
+          <SocialShareButtons href={STATIC_POST.href} />
 
           <button className="btn btn-dark">Mint</button>
         </section>
@@ -114,7 +114,7 @@ export default async function Page({ params }: PageProps) {
 
       <div className="prose-cover-image rounded-2xl overflow-hidden border border-gray-400 bg-slate-100 child-past-parent h-96 max-h-96">
         <Image
-          src={"/img/sample.jpg"}
+          src={STATIC_POST.image}
           fill={true}
           alt={"cover"}
           className="object-cover object-center"
@@ -125,7 +125,7 @@ export default async function Page({ params }: PageProps) {
       </div>
 
       <article className="prose max-w-full !text-lg">
-        full post content here
+        {STATIC_POST.description}
         {/* <MarkdownFormatter source={mdxSerialized} /> */}
       </article>
 
