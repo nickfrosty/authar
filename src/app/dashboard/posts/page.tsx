7166621b78c015b1f916b0d3cf52ab036c593dad
nickfrosty <nfrostbutter@gmail.com>
2023-11-18 19:55:45 -0500

    feat: added header to posts page

--- src/app/dashboard/posts/page.tsx
+++ src/app/dashboard/posts/page.tsx
@@ -1,9 +1,45 @@
 import { Metadata } from "next";
+import { DashboardSubHeader } from "@/components/dashboard/DashboardSubHeader";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Authar - Posts",
 };
 
 export default async function Page() {
-  return <>posts</>;
+  return (
+    <>
+      <DashboardSubHeader className="flex justify-between gap-2 items-center">
+        <section className="flex justify-between gap-2 items-center">
+          <input
+            type="text"
+            name="k"
+            id="search"
+            className="input-default"
+            placeholder="Search your posts..."
+          />
+        </section>
+
+        <section className="flex justify-between gap-2 items-center">
+          <Link
+            href="/dashboard/posts/new"
+            className="inline-block btn btn-dark"
+          >
+            Create Post
+          </Link>
+        </section>
+      </DashboardSubHeader>
+
+      <main className="container">
+        <p className="">listing of all posts</p>
+
+        <Link
+          href="/dashboard/posts/post-slug-to-test-editor-page"
+          className="inline-block btn btn-dark"
+        >
+          Edit a post
+        </Link>
+      </main>
+    </>
+  );
 }
