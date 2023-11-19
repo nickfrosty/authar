a7fb7c1a129865f27204f2dbff0890d6d6a7a701
nickfrosty <nfrostbutter@gmail.com>
2023-11-18 22:22:54 -0500

    feat: added a static user

---
+++ src/data.ts
@@ -0,0 +1,11 @@
+import AvatarPic from "@/../public/img/nick.jpg";
+
+/**
+ * A simple an reusable object with user profile type information
+ */
+export const STATIC_USER = {
+  name: "Nick Frostbutter",
+  username: "nickfrosty",
+  image: AvatarPic,
+  bio: "Nick is a self taught developer and submariner. He spends his free time writing docs and technical articles, as well as building side projects. He strives to one day make a living online from those projects.",
+};
