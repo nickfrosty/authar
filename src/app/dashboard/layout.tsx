264538c6a75bce525056a0d859c075d0812f47eb
nickfrosty <nfrostbutter@gmail.com>
2023-11-18 19:14:25 -0500

    feat: basic dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,20 @@
 import { Metadata } from "next";
+import { DashboardMenu } from "@/components/dashboard/sidebar/DashboardMenu";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export const metadata: Metadata = {
-  title: "Dashboard",
+  title: "Authar - Dashboard",
 };
 
 export default async function Layout({ children }: LayoutProps) {
-  return <>{children}</>;
+  return (
+    <section className="flex">
+      <DashboardMenu />
+
+      <main className="container flex-grow bg-white">{children}</main>
+    </section>
+  );
 }
