e819f466339b876199c41b4da527f8e2fa961a54
nickfrosty <nfrostbutter@gmail.com>
2023-11-19 11:39:06 -0500

    feat: display a header on the post page

--- src/app/[profile]/[post]/page.tsx
+++ src/app/[profile]/[post]/page.tsx
@@ -8,6 +8,7 @@ import { STATIC_POST } from "@/data";
 // import MarkdownFormatter from "@/components/MarkdownFormatter";
 import { SocialShareButtons } from "@/components/SocialButtons";
 import { FormattedDateAgo } from "@/components/core/FormattedDateAgo";
+import MarketingHeader from "@/components/marketing/MarketingHeader";
 
 type PageProps = {
   params: {
@@ -68,68 +69,72 @@ export default async function Page({ params }: PageProps) {
   // });
 
   return (
-    <main className="page-container max-w-3xl !space-y-4 md:!space-y-6">
-      <h1 className="font-bold text-3xl md:text-4xl max-w-5xl">
-        <Link href={STATIC_POST.href} className="">
-          {STATIC_POST.title}
-        </Link>
-      </h1>
+    <>
+      <MarketingHeader />
 
-      <section className="flex items-center justify-between gap-4">
-        <section className="flex items-center gap-2 md:gap-4">
-          <Link
-            href={`/${STATIC_POST.author.username}`}
-            className="block rounded-full overflow-hidden w-12 h-12 bg-slate-300"
-          >
-            <Image
-              width={64}
-              height={64}
-              src={STATIC_POST.author.image}
-              alt={STATIC_POST.author.name}
-              className="object-cover object-center rounded-full overflow-hidden w-12 h-12"
-              priority={true}
-            />
+      <main className="page-container max-w-3xl !space-y-4 md:!space-y-6">
+        <h1 className="font-bold text-3xl md:text-4xl max-w-5xl">
+          <Link href={STATIC_POST.href} className="">
+            {STATIC_POST.title}
           </Link>
+        </h1>
 
-          <div className="space-y-0">
+        <section className="flex items-center justify-between gap-4">
+          <section className="flex items-center gap-2 md:gap-4">
             <Link
               href={`/${STATIC_POST.author.username}`}
-              className="hover:underline md:text-lg font-medium"
+              className="block rounded-full overflow-hidden w-12 h-12 bg-slate-300"
             >
-              {STATIC_POST.author.name}
+              <Image
+                width={64}
+                height={64}
+                src={STATIC_POST.author.image}
+                alt={STATIC_POST.author.name}
+                className="object-cover object-center rounded-full overflow-hidden w-12 h-12"
+                priority={true}
+              />
             </Link>
 
-            <FormattedDateAgo date={STATIC_POST.date} />
-          </div>
+            <div className="space-y-0">
+              <Link
+                href={`/${STATIC_POST.author.username}`}
+                className="hover:underline md:text-lg font-medium"
+              >
+                {STATIC_POST.author.name}
+              </Link>
 
-          <button className="btn mx-8 border-gray-400">Following</button>
+              <FormattedDateAgo date={STATIC_POST.date} />
+            </div>
+
+            <button className="btn mx-8 border-gray-400">Following</button>
+          </section>
+
+          <section className="flex items-center gap-3">
+            <SocialShareButtons href={STATIC_POST.href} />
+
+            <button className="btn btn-dark">Mint</button>
+          </section>
         </section>
 
-        <section className="flex items-center gap-3">
-          <SocialShareButtons href={STATIC_POST.href} />
+        <div className="prose-cover-image rounded-2xl overflow-hidden border border-gray-400 bg-slate-100 child-past-parent h-96 max-h-96">
+          <Image
+            src={STATIC_POST.image}
+            fill={true}
+            alt={"cover"}
+            className="object-cover object-center"
+            // note: this image will likely be the largest content paint,
+            // so setting priority is good for page load speed
+            priority={true}
+          />
+        </div>
 
-          <button className="btn btn-dark">Mint</button>
-        </section>
-      </section>
+        <article className="prose max-w-full !text-lg">
+          {STATIC_POST.description}
+          {/* <MarkdownFormatter source={mdxSerialized} /> */}
+        </article>
 
-      <div className="prose-cover-image rounded-2xl overflow-hidden border border-gray-400 bg-slate-100 child-past-parent h-96 max-h-96">
-        <Image
-          src={STATIC_POST.image}
-          fill={true}
-          alt={"cover"}
-          className="object-cover object-center"
-          // note: this image will likely be the largest content paint,
-          // so setting priority is good for page load speed
-          priority={true}
-        />
-      </div>
-
-      <article className="prose max-w-full !text-lg">
-        {STATIC_POST.description}
-        {/* <MarkdownFormatter source={mdxSerialized} /> */}
-      </article>
-
-      {/* <AboutTheAuthor /> */}
-    </main>
+        {/* <AboutTheAuthor /> */}
+      </main>
+    </>
   );
 }
