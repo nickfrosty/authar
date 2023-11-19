e1f6d8fc79df3c1b8e4d11bbe8797212823bb8f9
nickfrosty <nfrostbutter@gmail.com>
2023-11-19 17:10:04 -0500

    feat: added debounce helper

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -53,3 +53,17 @@ export function computePagination({
 
   return data;
 }
+
+/**
+ *
+ */
+export function debounce<F extends (...params: any[]) => void>(
+  fn: F,
+  delay: number,
+) {
+  let timeoutID: number | undefined;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutID);
+    timeoutID = window.setTimeout(() => fn.apply(this, args), delay);
+  } as F;
+}
