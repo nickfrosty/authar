cb81261d4eea2031ed2dbb85bbce50ce7bcc151a
nickfrosty <nfrostbutter@gmail.com>
2023-11-18 22:43:15 -0500

    refactor: removed comments

--- src/app/[profile]/layout.tsx
+++ src/app/[profile]/layout.tsx
@@ -1,8 +1,3 @@
-// import { notFound } from "next/navigation";
-// import { Metadata, ResolvingMetadata } from "next";
-// import { getUserProfile } from "@/lib/queries/users";
-// import ProfileFooter from "@/components/profiles/ProfileFooter";
-
 type LayoutProps = {
   children: React.ReactNode;
   params: {
@@ -10,42 +5,9 @@ type LayoutProps = {
   };
 };
 
-// export async function generateMetadata(
-//   { params }: LayoutProps,
-//   parent: ResolvingMetadata,
-// ): Promise<Metadata> {
-//   if (!params.profile) notFound();
-
-//   // get the profile record from the database
-//   const profile = await getUserProfile({
-//     username: params.profile,
-//   });
-
-//   // handle the 404 for no or invalid `profile`
-//   if (!profile) notFound();
-
-//   return {
-//     title: profile.title
-//       ? profile.title
-//       : profile.name
-//       ? `${profile.name} (@${profile.username})`
-//       : `@${profile.username}'`,
-//     description:
-//       profile.bio ??
-//       profile.oneLiner ??
-//       `${profile.name} (@${profile.username})`,
-//     alternates: {
-//       canonical: `/${profile.username}`,
-//     },
-//   };
-// }
+// todo: should we check for the existence of the `profile` here?
+// it may help prevent trickle down requests on child pages
 
 export default async function Layout({ children }: LayoutProps) {
-  return (
-    <>
-      {children}
-
-      {/* <ProfileFooter /> */}
-    </>
-  );
+  return <>{children}</>;
 }
