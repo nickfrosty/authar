b2e7359c00d1eef17e056c5b77f13abfaddfc0b3
nickfrosty <nfrostbutter@gmail.com>
2023-11-19 21:23:34 -0500

    feat: added editor util

--- src/components/editor/utils.ts
+++ src/components/editor/utils.ts
@@ -1,12 +1,35 @@
 import { $isRangeSelection, EditorState, RangeSelection } from "lexical";
+import { $isLinkNode } from "@lexical/link";
 
 /**
  * Check if there is a selection that spans across
  * content, i.e. is not empty
  */
-
 export function $isRangeSelected(
   selection: EditorState["_selection"],
 ): selection is RangeSelection {
   return $isRangeSelection(selection) && !selection.anchor.is(selection.focus);
+}
+
+/**
+ * Check if all nodes in the selection share the same link target.
+ * If so, return the link target, otherwise return undefined.
+ */
+export function $getSharedLinkTarget(
+  selection?: EditorState["_selection"],
+): string | undefined {
+  const nodes = selection?.getNodes();
+  if (!nodes?.length) return undefined;
+
+  const sharedLinkTarget = nodes.every((node, i, arr) => {
+    const parent = node.getParent();
+    if (!$isLinkNode(parent)) return false;
+
+    const linkTarget = parent.getURL();
+    const prevLinkTarget = arr[i - 1]?.getParent()?.getURL();
+
+    return i > 0 ? linkTarget === prevLinkTarget : true;
+  });
+
+  return sharedLinkTarget ? nodes[0].getParent()?.getURL() : undefined;
 }
