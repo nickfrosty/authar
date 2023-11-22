cf3e7d05622982e1a6756143d73d4d8f5c66516e
nickfrosty <nfrostbutter@gmail.com>
2023-11-21 21:21:01 -0500

    feat: added href prop for logo

--- src/components/core/AppLogo.tsx
+++ src/components/core/AppLogo.tsx
@@ -9,17 +9,20 @@ import LogoMarkBlack from "@/../public/logomark-black.svg";
 
 type AppLogoProps = SimpleComponentProps & {
   height?: number;
+  href?: string;
 };
 
-export const AppLogo = memo(({ className = "", height = 40 }: AppLogoProps) => {
-  return (
-    <Link
-      href={"/"}
-      className={`font-bold text-2xl inline-flex gap-2 ${className}`}
-    >
-      {/* <Image src={LogoBlack} alt={""} height={40} priority />
+export const AppLogo = memo(
+  ({ className = "", height = 40, href = "/" }: AppLogoProps) => {
+    return (
+      <Link
+        href={href}
+        className={`font-bold text-2xl inline-flex gap-2 ${className}`}
+      >
+        {/* <Image src={LogoBlack} alt={""} height={40} priority />
       <Image src={WordmarkBlack} alt={SITE.name} height={28} priority /> */}
-      <Image src={LogoMarkBlack} alt={SITE.name} height={height} priority />
-    </Link>
-  );
-});
+        <Image src={LogoMarkBlack} alt={SITE.name} height={height} priority />
+      </Link>
+    );
+  },
+);
