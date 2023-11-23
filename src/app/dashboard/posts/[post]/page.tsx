2ba214c7facbf05185e906c2013b0631a2ae6ab3
nickfrosty <nfrostbutter@gmail.com>
2023-11-23 09:37:23 -0500

    feat: editor UI

--- src/app/dashboard/posts/[post]/page.tsx
+++ src/app/dashboard/posts/[post]/page.tsx
@@ -1,12 +1,9 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { DashboardSubHeader } from "@/components/dashboard/DashboardSubHeader";
-import Link from "next/link";
-import { FeatherIcon } from "@/components/core/FeatherIcon";
-import Editor from "@/components/editor";
-import clsx from "clsx";
 
-import { EditorMenu } from "@/components/dashboard/sidebar/EditorMenu";
+import Editor from "@/components/editor";
+import { EditorMenu } from "@/components/dashboard/editor/EditorMenu";
+import { EditorHeader } from "@/components/dashboard/editor/EditorHeader";
 
 type PageProps = {
   params: {
@@ -26,42 +23,16 @@ export default async function Page({ params }: PageProps) {
   if (!post) notFound();
 
   return (
-    <>
-      <DashboardSubHeader className="flex justify-between gap-2 items-center">
-        <Link
-          href="/dashboard/posts"
-          className="hover:underline flex justify-between gap-2 items-center"
-        >
-          <FeatherIcon name="ArrowLeft" strokeWidth={1.8} size={20} />
-          All posts
-        </Link>
+    <main className="md:flex justify-between flex-row">
+      <section className="!flex-grow !max-w-full py-4 px-6 space-y-4">
+        <EditorHeader />
 
-        <section className="flex justify-between gap-2 items-center">
-          <Link
-            target="_blank"
-            href="/user_profile/post-slug-or-id-here"
-            className="inline-flex btn btn-ghost"
-          >
-            Preview
-            <FeatherIcon name="ExternalLink" size={16} />
-          </Link>
-
-          <button type="button" className="inline-block btn btn-dark">
-            Publish
-          </button>
-        </section>
-      </DashboardSubHeader>
-
-      <main className="md:flex gap-8 justify-between flex-row">
-        <article
-          className="!flex-grow !max-w-full py-4 px-6 prose"
-          id={"lexical-editor"}
-        >
+        <article className="!flex-grow !max-w-full prose" id={"lexical-editor"}>
           <Editor editorId="lexical-editor" />
         </article>
+      </section>
 
-        <EditorMenu />
-      </main>
-    </>
+      <EditorMenu />
+    </main>
   );
 }
