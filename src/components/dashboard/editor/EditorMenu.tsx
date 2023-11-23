92958a4807a6db6b6a76cbdbcacdcbbd996614fe
nickfrosty <nfrostbutter@gmail.com>
2023-11-23 10:21:08 -0500

    feat: editor menu styles

--- src/components/dashboard/editor/EditorMenu.tsx
+++ src/components/dashboard/editor/EditorMenu.tsx
@@ -4,7 +4,8 @@ import { memo, useState } from "react";
 import clsx from "clsx";
 
 import { FeatherIcon } from "@/components/core/FeatherIcon";
-import formStyles from "@/styles/Forms.module.css";
+import form from "@/styles/Forms.module.css";
+import styles from "@/styles/dashboard/editor/EditorMenu.module.css";
 
 type EditorMenuProps = {
   className?: string;
@@ -17,61 +18,63 @@ export const EditorMenu = memo(({ className = "" }: EditorMenuProps) => {
     <aside
       className={clsx(
         className,
-        "flex flex-col flex-shrink-0 top-0 left-0 space-y-4 py-4 px-6 md:pt-4",
-        "w-full h-screen min-h-screen max-h-screen overflow-y-auto",
-        "border-l border-gray-300",
-        "md:w-96 sticky",
+        styles.menu,
         // support the collapsible sidebar for a cleaner dashboard UI
         // !!showMenu && "md:max-w-fit",
-        //
-        showMenu ? "h-screen" : "hidden md:flex",
-
-        // " items-start gap-4 space-y-4",
+        showMenu ? styles.active : styles.inactive,
       )}
     >
-      <section className="flex items-center justify-between gap-2">
-        <h2 className="font-semibold text-xl">Post settings</h2>
+      <header className={styles.topper}>
+        <h2>Post settings</h2>
 
         <button
           type="button"
-          className="p-2"
+          className={styles.button}
           onClick={() => setShowMenu(!showMenu)}
         >
           <FeatherIcon
-            name="LogOut"
-            className={clsx(
-              "flex-shrink-0 place-self-center",
-              showMenu && "rotate-180",
-            )}
+            name="X"
+            // className={showMenu ? "rotate-180" : ""}
           />
         </button>
-      </section>
+      </header>
 
-      {/* <ul className="space-y-1 flex-grow"> */}
-
-      <section className="">
-        <div className={formStyles.element}>
-          <label htmlFor="slug">Post URL</label>
-
-          <div className="relative">
-            <FeatherIcon
-              name="Link"
-              strokeWidth={2}
-              className="text-gray-500 absolute top-2.5 left-3 w-5 h-5"
-            />
+      <section className={styles.section}>
+        <FormItem
+          label="Post URL"
+          description={`authar.io/username/post-slug-here`}
+        >
+          <div className={styles.elementWithIcon}>
+            <FeatherIcon name="Link" strokeWidth={1.8} />
             <input
               type="text"
               name="slug"
               id="slug"
-              className="!pl-9"
               placeholder="Customize this post URL"
             />
           </div>
-          <p className={formStyles.description}>
-            authar.io/username/post-slug-here
-          </p>
-        </div>
+        </FormItem>
       </section>
     </aside>
+  );
+});
+
+type FormItemProps = {
+  label: string;
+  description?: string;
+  children: React.ReactNode;
+};
+
+const FormItem = memo(({ label, description, children }: FormItemProps) => {
+  return (
+    <div className={form.element}>
+      <label htmlFor="slug">{label}</label>
+
+      {children}
+
+      {typeof description != "undefined" && (
+        <p className={form.description}>{description}</p>
+      )}
+    </div>
   );
 });
