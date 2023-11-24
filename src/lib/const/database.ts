3d608470212ed47196e10c34e99acffe09705d9e
nickfrosty <nfrostbutter@gmail.com>
2023-11-24 10:28:10 -0500

    feat: added more blacklisted words

--- src/lib/const/database.ts
+++ src/lib/const/database.ts
@@ -16,9 +16,39 @@ export const USERNAME_BLACKLIST = [
   "help",
   "blog",
   "dashboard",
+  "settings",
+  "account",
+  "manage",
   "features",
   "login",
   "logout",
   "signin",
   "signout",
+];
+
+/**
+ * List of post slugs that are not allowed to be created
+ *
+ * todo: should this include the list of blacklisted usernames as well?
+ * when subdomains and custom domains are supported, these slug (and usernames)
+ * will likely be used for root page routes
+ */
+export const POST_SLUG_BLACKLIST = [
+  // comment for better diffs
+  "edit",
+  "archive",
+  "coffee",
+  "tip",
+  "tags",
+  "search",
+  "followers",
+  "members",
+  "message",
+  "messages",
+  "chat",
+  "dm",
+  "subscribers",
+  "posts",
+  "comments",
+  "likes",
 ];
