ad656ac38600d2dcd1fe0eb08eff0ea5c5b9dba6
nickfrosty <nfrostbutter@gmail.com>
2023-11-24 09:23:12 -0500

    feat: added username blacklist

---
+++ src/lib/const/database.ts
@@ -0,0 +1,24 @@
+/**
+ * Assorted constants used in tandem with the database
+ */
+
+/**
+ * List of usernames that are not allowed to be created
+ *
+ * note: this list should include the page routes that the site uses
+ */
+export const USERNAME_BLACKLIST = [
+  // comment for better diffs
+  "authar",
+  "author",
+  "admin",
+  "api",
+  "help",
+  "blog",
+  "dashboard",
+  "features",
+  "login",
+  "logout",
+  "signin",
+  "signout",
+];
