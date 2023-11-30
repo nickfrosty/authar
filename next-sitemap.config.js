e69b82921358d18f068211e32db3b6d04ab7d8f2
nickfrosty <nfrostbutter@gmail.com>
2023-11-30 05:57:46 -0500

    feat: sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,21 @@
-const SITE_URL = "https://solfate.com";
+const SITE_URL = "https://authar";
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: SITE_URL,
   generateRobotsTxt: true,
   exclude: [
+    "/dashboard*",
+    "/api*",
     // "/sitemap-generated.xml", // <= exclude custom dynamic sitemap
   ],
   robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: ["/api", "/dashboard"],
+      },
+    ],
     additionalSitemaps: [
       // `${SITE_URL}/sitemap-generated.xml`, // add custom dynamic sitemap
     ],
